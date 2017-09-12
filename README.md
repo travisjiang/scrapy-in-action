1f2862f5febaed283768494348231794f0911238
travis <jty@ubuntu>
2017-09-12 08:07:34 -0700

    add huaban meinv

---
+++ README.md
@@ -0,0 +1,4 @@
+用于记录一些简单地爬虫
+
+* jiandan：基于scrapy实现到，用于爬取煎蛋网到图保存到本地
+* huaban:基于https://github.com/pfchai/Crawler-Demo/blob/master/huaban/python/HuabanCrawler.py, 图片链接时奇葩到拼接方式获取，可参见blog：http://www.cnblogs.com/nan86150/p/4272452.html
---
+++ huaban/crawler.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# coding=utf-8
+#!/usr/bin/env python
+# encoding: utf-8
+# @author: cc <chai_pengfei@163.com>
+
+import requests
+import re
+import os
+import os.path
+
+class HuabanCrawler():
+    """ 抓去花瓣网上的图片 """
+
+    def __init__(self):
+        """ 在当前文件夹下新建images文件夹存放抓取的图片 """
+        #self.homeUrl = "http://huaban.com/favorite/beauty/"
+        self.homeUrl = "http://huaban.com/boards/29442728/"
+        self.images = []
+        self.save_path  = "/mnt/hgfs/vmshare/crawler/huaban"
+        if not os.path.exists(self.save_path):
+            os.mkdir(self.save_path)
+
+    def __load_homePage(self):
+        """ 加载主页面 """
+        return requests.get(url = self.homeUrl).content
+
+    def __make_ajax_url(self, No):
+        """ 返回ajax请求的url """
+        return self.homeUrl + "?i5p998kw&max=" + No + "&limit=20&wfl=1"
+
+    def __load_more(self, maxNo):
+        """ 刷新页面 """
+        return requests.get(url = self.__make_ajax_url(maxNo)).content
+
+    def __process_data(self, htmlPage):
+        """ 从html页面中提取图片的信息 """
+        #与http://huaban.com/favorite/beauty页面到解析是不同的
+        prog = re.compile(r'app\.page\["board"\].*')
+        appPins = prog.findall(htmlPage)
+        # 将js中的null定义为Python中的None
+        null = None
+        true = True
+        false = False
+        #import pdb
+        #pdb.set_trace()
+        if appPins == []:
+            return None
+        result = eval(appPins[0][20:-1])
+        pins = result["pins"]
+        for i in pins:
+            info = {}
+            info['id'] = str(i['pin_id'])
+            info['url'] = "http://img.hb.aicdn.com/" + i["file"]["key"] + "_fw658"
+            print(info['url'])
+            if 'image' == i["file"]["type"][:5]:
+                info['type'] = i["file"]["type"][6:]
+            else:
+                info['type'] = 'NoName'
+            self.images.append(info)
+
+    def __save_image(self, imageName, content):
+        """ 保存图片 """
+        with open(imageName, 'wb') as fp:
+            fp.write(content)
+
+    def get_image_info(self, num=20):
+        """ 得到图片信息 """
+        self.__process_data(self.__load_homePage())
+        for i in range((num-1)/20):
+            self.__process_data(self.__load_more(self.images[-1]['id']))
+        return self.images
+
+    def down_images(self):
+        """ 下载图片 """
+        print "{} image will be download".format(len(self.images))
+        for key, image in enumerate(self.images):
+            print 'download {0} ...'.format(key)
+            try:
+                req = requests.get(image["url"])
+            except :
+                print 'error'
+            imageName = os.path.join(self.save_path, image["id"] + "." + image["type"])
+            self.__save_image(imageName, req.content)
+
+
+if __name__ == '__main__':
+    hc = HuabanCrawler()
+    hc.get_image_info(4000)
+    hc.down_images()
